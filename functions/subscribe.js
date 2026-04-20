dc5d1554ae0853990ff218067a9cf594a703edf2
tedity967 <50928680+tedity967@users.noreply.github.com>
2026-04-20 17:54:39 +0100

    Update subscribe.js

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -1,5 +1,5 @@
 export async function onRequestPost(context) {
-  const BREVO_API_KEY = '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';
+  const BREVO_API_KEY = context.env.BREVO_API_KEY;
  
   let body;
   try {
@@ -53,4 +53,3 @@ export async function onRequestOptions() {
     }
   });
 }
- 
